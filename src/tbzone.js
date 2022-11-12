a4172bddf47fcc966abf0bf9af3b6e18f087f67a
sturdoodle <113435474+sturdoodle@users.noreply.github.com>
2022-11-12 18:14:55 +0530

    update

--- src/tbzone.js
+++ src/tbzone.js
@@ -8,10 +8,21 @@ export default class tbzone extends Component {
   render() {
     // const { router, params, location, routes } = this.props;
     const {location} = this.props;
+    var filepathname = "";
+    if(location.pathname.includes("1210-0")){
+      filepathname=location.pathname.replace("1210-0","")
+      filepathname=filepathname.replace("/","")
+      // https://drive.google.com/file/d/1_8b1Fxwx3LT0uAtSdiAB3JKf8NYfbPhX/view
+      // https://drive.google.com/uc?id=1_8b1Fxwx3LT0uAtSdiAB3JKf8NYfbPhX&confirm=t
+      // filepathname=`https://drive.google.com/uc?id=${filepathname}/view`
+      filepathname=`https://drive.google.com/file/d/${filepathname}/view`
+    }
+    else{
+      // https://drive.google.com/uc?export=download&id=1_8b1Fxwx3LT0uAtSdiAB3JKf8NYfbPhX
+      filepathname=location.pathname.replace("/","")
+      filepathname=`https://drive.google.com/uc?export=download&id=${filepathname}`;
 
-    const pt1name = location.pathname;
-    const filepathname = `https://shtrlnk.ml${pt1name}`;
-    // console.log(filepathname)
+    }
     return (
       <>
         <Header />
